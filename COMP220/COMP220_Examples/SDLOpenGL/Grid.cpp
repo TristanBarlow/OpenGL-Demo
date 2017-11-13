a3f860ee40385083dcf5cb7c3c9eb5db702e345c
tristanbarlow <tristanbarlowg@gmail.com>
2017-11-13 16:28:33 +0000

    s

--- COMP220/COMP220_Examples/SDLOpenGL/Grid.cpp
+++ COMP220/COMP220_Examples/SDLOpenGL/Grid.cpp
@@ -21,11 +21,11 @@ void Grid::createGridVec(int numberX, int numberY, GLuint programID)
 			vec3 lineVert4 = vec3(0, -1, j);
 			vec4 col = vec4(0.5, 0.5, 0.5, 1.0);
 			vec4 col1 = vec4(0.5, 0.5, 0.5, 1.0);
-			if (j%10== 0)
+			if (j%10==0 || j== startPosY)
 			{
 				col = vec4(0.1, 1.0, 0.1, 1.0);
 			}
-			if (i%10 == 0)
+			if (i%10 == 0 || i == startPosX)
 			{
 				col1 = vec4(0.1, 1.0, 0.1, 1.0);
 			}
